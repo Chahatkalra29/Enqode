3e2be87e9e8f895c55527e841640ca24dcc0a624
Chahatkalra29 <Kalrachahat0@gmail.com>
2025-04-25 23:01:12 +0530

    Trying some qoutes for Enqode

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -8,6 +8,7 @@ const Dashboard = () => {
         <title>Dashboard-Enqode</title>
       </Helmet>
       <h2 className="text-blue-500 text-5xl font-bold flex justify-center h-screen items-center">Welcome to Enqode</h2>
+      <h2 className="text-blue-600 text-5xl font-bold flex justify-center h-screen items-center">Enqode – Quick. Clean. QR.</h2>   
     </>
   );
 };
