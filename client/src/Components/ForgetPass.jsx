19be1a0293d8a00dfeda27d735a422158d3535d5
Chahatkalra29 <Kalrachahat0@gmail.com>
2025-04-27 23:13:58 +0530

    New files for forget password and restting password

---
+++ client/src/Components/ForgetPass.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Helmet } from "react-helmet-async";
+
+const ForgetPass = () => {
+  return (
+    <div>
+    <Helmet>
+            <title>Forgot Password-Enqode</title>
+          </Helmet>
+          ForgetPass</div>
+  )
+}
+
+export default ForgetPass
---
+++ client/src/Components/ResetPass.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Helmet } from "react-helmet-async";
+
+const ResetPass = () => {
+  return (
+    <div>
+    <Helmet>
+            <title>Reset Password-Enqode</title>
+          </Helmet>ResetPass</div>
+  )
+}
+
+export default ResetPass
