91746e417075f4cca4a8eb624f427fcce3ecf864
Chahatkalra29 <Kalrachahat0@gmail.com>
2025-05-06 22:35:02 +0530

    Register page ui start, login page bg

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -4,13 +4,12 @@ import axios from "axios";
 import { toast, ToastContainer, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Helmet } from "react-helmet-async";
-import logo from "../assets/logo-transparent-bg.png"
+import logo from "../assets/logo-transparent-bg.png";
 
 const Login = () => {
-  const navigate = useNavigate()
-  const[error,setError]= useState("")
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
   const [errorTrigger, setErrorTrigger] = useState(false);
- 
 
   const [userDetails, setUserDetails] = useState({
     uemail: "",
@@ -35,10 +34,10 @@ const Login = () => {
       console.log(loginDetails);
       if (loginDetails.data.loginsts === "0") {
         localStorage.setItem("utoken", loginDetails.data.token);
-        navigate('/dashboard')
+        navigate("/dashboard");
       } else {
-        console.log("Error from server:", loginDetails.data.msg)
-        setError(loginDetails.data.msg)
+        console.log("Error from server:", loginDetails.data.msg);
+        setError(loginDetails.data.msg);
         setErrorTrigger((prev) => !prev);
       }
       // const token = localStorage.getItem("utoken")
@@ -66,16 +65,18 @@ const Login = () => {
         transition: Bounce,
       });
 
-      setError(""); 
+      setError("");
     }
   }, [errorTrigger]);
 
   return (
-    <div className="flex justify-center items-center  min-h-screen bg-lav relative overflow-hidden">
-    <div className="absolute inset-0 bg-grid-pattern opacity-10"></div>               
-    <Helmet>
-      <title>Login-Enqode</title>
-    </Helmet>
+    <div className="flex justify-center items-center  min-h-screen bg-txt-dark relative overflow-hidden">
+      <div className="absolute bottom-0 left-0 w-72 h-72 bg-[#D6D4FF]/10 rounded-full filter blur-[120px] animate-pulse"></div>
+      <div className="absolute top-0 right-0 w-72 h-72 bg-[#6C63FF]/10 rounded-full filter blur-[120px] animate-pulse"></div>
+
+      <Helmet>
+        <title>Login-Enqode</title>
+      </Helmet>
       <ToastContainer
         position="top-center"
         autoClose={5000}
@@ -92,39 +93,47 @@ const Login = () => {
       <div className="absolute bottom-0 left-0 w-96 h-96 bg-lav rounded-full filter blur-3xl"></div>
       <div className="absolute top-0 right-0 w-96 h-96 bg-lav rounded-full filter blur-3xl"></div>
       <div className="h-2/3 p-8 w-1/4  bg-txt-dark rounded-2xl space-y-3 shadow-xl border border-txt-dark">
-      <div className="flex flex-col items-center mb-6">
-        <img 
-                className="h-17 w-17" 
-                src={logo} 
-                alt="Enqode Logo" 
-              />
-        <h1 className="text-2xl font-bold text-bg-light">Welcome Back</h1>
-        <p className="text-bg-light mt-1">Log in to your Enqode account</p>
+        <div className="flex flex-col items-center mb-6">
+          <img className="h-17 w-17" src={logo} alt="Enqode Logo" />
+          <h1 className="text-2xl font-bold text-bg-light">Welcome Back</h1>
+          <p className="text-bg-light mt-1">Log in to your Enqode account</p>
         </div>
-        <div> <input
-          type="email"
-          name="uemail"
-          placeholder="Enter your registered email"
-          required
-          className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"
-          onChange={handleInputChange}
-        />
+        <div>
+          <label htmlFor="uemail" className="block text-sm text-gray-300 mb-1">
+            Email
+          </label>{" "}
+          <input
+            type="email"
+            name="uemail"
+            placeholder="Enter your registered email"
+            required
+            className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"
+            onChange={handleInputChange}
+          />
+        </div>
 
+        <div>
+          <label htmlFor="upass" className="block text-sm text-gray-300 mb-1">
+            Password
+          </label>
+          <input
+            type="password"
+            name="upass"
+            placeholder="Password"
+            required
+            className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"
+            onChange={handleInputChange}
+          />
         </div>
-       
-        <div className="flex items-center justify-between mb-1">
-        <Link to={"/forgetPass"} className="text-royal-blue text-xs hover:text-royal-blue/80">
-          Forgot Password?{" "}
-        </Link>
+        <div className="flex justify-end">
+          <Link
+            to={"/forgetPass"}
+            className="text-royal-blue text-xs hover:text-royal-blue/80"
+          >
+            Forgot Password?{" "}
+          </Link>
         </div>
-        <input
-          type="password"
-          name="upass"
-          placeholder="Password"
-          required
-          className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"
-          onChange={handleInputChange}
-        />
+
         <button
           className=" w-full py-3 mt-2 bg-royal-blue hover:from-lav hover:to-royal-blue rounded-xl text-bg-light font-bold tracking-wide shadow-lg hover:shadow-cyan-500/50 transition-all duration-300"
           onClick={handleFormSubmit}
@@ -138,14 +147,16 @@ const Login = () => {
           Test Toast
         </button> */}
         <div className="text-center mt-6">
-        <p className="text-sm text-gray-400">Don't have an account yet?</p>
-        <Link to={"/register"} className="text-royal-blue hover:text-royal-blue/50 ">
-          Register here{" "}
-        </Link>
+          <p className="text-sm text-gray-400">Don't have an account yet?</p>
+          <Link
+            to={"/register"}
+            className="text-royal-blue hover:text-royal-blue/50 "
+          >
+            Register here{" "}
+          </Link>
         </div>
-       
-        <br /> 
-        
+
+        <br />
       </div>
     </div>
   );
--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { Helmet } from "react-helmet-async";
+import logo from "../assets/logo-transparent-bg.png";
 
 const Register = () => {
   const [userDetails, setUserDetails] = useState({
@@ -19,24 +20,36 @@ const Register = () => {
   };
 
   const handleFormSubmit = async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/userapi/reguser", userDetails);
-      console.log(res.data); 
+      const res = await axios.post(
+        "http://localhost:5000/userapi/reguser",
+        userDetails
+      );
+      console.log(res.data);
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-    <Helmet>
-          <title>Register-Enqode</title>
-        </Helmet>
-      <div className="h-2/3 p-8 w-1/4 bg-[#151525] rounded-2xl space-y-3 shadow-xl border border-[#1f1f38]">
-        <h2 className="text-2xl mt-3 font-semibold text-cyan-400 tracking-widest text-center">
-          ENQODE
-        </h2>
+    <div className="flex justify-center items-center  min-h-screen bg-txt-dark relative overflow-hidden">
+      <div className="absolute bottom-0 left-0 w-72 h-72 bg-[#D6D4FF]/10 rounded-full filter blur-[120px] animate-pulse"></div>
+      <div className="absolute top-0 right-0 w-72 h-72 bg-[#6C63FF]/10 rounded-full filter blur-[120px] animate-pulse"></div>
+
+      <Helmet>
+        <title>Register-Enqode</title>
+      </Helmet>
+      <div className="absolute bottom-0 left-0 w-96 h-96 bg-lav rounded-full filter blur-3xl"></div>
+      <div className="absolute top-0 right-0 w-96 h-96 bg-lav rounded-full filter blur-3xl"></div>
+      <div className="h-2/3 p-8 w-1/4  bg-txt-dark rounded-2xl space-y-3 shadow-xl border border-txt-dark">
+      <div className="flex flex-col items-center mb-6">
+          <img className="h-17 w-17" src={logo} alt="Enqode Logo" />
+          <h1 className="text-2xl font-bold text-bg-light">Create Account</h1>
+          <p className="text-bg-light mt-1">
+            Join Enqode and start creating QR codes
+          </p>
+        </div>
         <input
           type="text"
           name="uname"
@@ -65,10 +78,10 @@ const Register = () => {
         >
           Register
         </button>
-        <p className="text-gray-400">Already have an account?</p>
-        <Link to="/login" className="text-blue-500 hover:underline">
+        <div className="text-center mt-6"><p className="text-sm text-gray-400">Already have an account?</p>
+        <Link to="/login" className="text-royal-blue hover:underline">
           Login here
-        </Link>
+        </Link></div>
         
       </div>
     </div>
