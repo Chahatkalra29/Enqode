5cc597c323c83d2d0f46591d27a55a3e97afbe85
Chahatkalra29 <Kalrachahat0@gmail.com>
2025-06-23 20:02:05 +0530

    vercel

--- server/App.js
+++ server/App.js
@@ -1,33 +1,38 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv').config()
-const mongoose = require('mongoose')
-require('events').EventEmitter.defaultMaxListeners = 20;
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+require("events").EventEmitter.defaultMaxListeners = 20;
 
 // const userModel = require('./Models/User.jsx')
 // http://localhost:5000/userapi/testuser
-const userRouter = require('./Routes/UserRoute')
+const userRouter = require("./Routes/UserRoute");
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "https://enqode-git-main-chahat-kalras-projects.vercel.app",
+    credentials: true,
+  })
+);
 
-app.use('/userapi',userRouter)
+app.use("/userapi", userRouter);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port,(req,res)=>{
-    console.log(`Server is running on: http://localhost:${port}`)
-})
-mongoose.connect(process.env.DB_URL)
+app.listen(port, (req, res) => {
+  console.log(`Server is running on: http://localhost:${port}`);
+});
+mongoose.connect(process.env.DB_URL);
 
-mongoose.connection.on("connected",()=>{
-    console.log("MongoDB connected successfully")
-})
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected successfully");
+});
 
-mongoose.connection.on("error",(error)=>{
-    console.log(`It is a ${error}, please try again.`)
-})
+mongoose.connection.on("error", (error) => {
+  console.log(`It is a ${error}, please try again.`);
+});
 
 module.exports = mongoose;
