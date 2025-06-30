5fcf4320537a59305136dde650e5d2267435e605
Chahatkalra29 <Kalrachahat0@gmail.com>
2025-06-30 14:25:01 +0530

    registeration error toast

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { Helmet } from "react-helmet-async";
 import logo from "../assets/logo-transparent-bg.png";
+import { toast, ToastContainer, Bounce } from "react-toastify";
 
 const Register = () => {
-    const backendUrl= import.meta.env.VITE_BACKEND_URL
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [userDetails, setUserDetails] = useState({
     uname: "",
     email: "",
@@ -23,13 +24,15 @@ const Register = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(
-        `${backendUrl}userapi/reguser`,
-        userDetails
-      );
+      const res = await axios.post(`${backendUrl}userapi/reguser`, userDetails);
       console.log(res.data);
     } catch (error) {
       console.error(error);
+      toast.error("Server error. Please try again later.", {
+        position: "top-center",
+        theme: "dark",
+        transition: Bounce,
+      });
     }
   };
 
@@ -41,43 +44,67 @@ const Register = () => {
       <Helmet>
         <title>Register-Enqode</title>
       </Helmet>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
       <div className="absolute bottom-0 left-0 w-96 h-96 bg-lav/10 rounded-full filter blur-3xl"></div>
       <div className="absolute top-0 right-0 w-96 h-96 bg-lav/10   rounded-full filter blur-3xl"></div>
       <div className="h-2/3 p-8 w-1/4  bg-txt-dark rounded-2xl space-y-3 shadow-xl border border-bg-light/10">
-      <div className="flex flex-col items-center mb-6">
+        <div className="flex flex-col items-center mb-6">
           <img className="h-17 w-17" src={logo} alt="Enqode Logo" />
-          <h1 className="text-2xl font-bold text-bg-light tracking-wider">Create Account</h1>
+          <h1 className="text-2xl font-bold text-bg-light tracking-wider">
+            Create Account
+          </h1>
           <p className="text-bg-light mt-1">
             Join Enqode and start creating QR codes
           </p>
         </div>
-        <div><label htmlFor="uname" className="block text-sm text-gray-300 mb-1">
+        <div>
+          <label htmlFor="uname" className="block text-sm text-gray-300 mb-1">
             Full name
           </label>
           <input
-          type="text"
-          name="uname"
-          placeholder="Enter your Name"
-          required
-          onChange={handleInputChange}
-          className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"  /></div>
-        <div><label htmlFor="email" className="block text-sm text-gray-300 mb-1">
+            type="text"
+            name="uname"
+            placeholder="Enter your Name"
+            required
+            onChange={handleInputChange}
+            className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="block text-sm text-gray-300 mb-1">
             Email
-          </label><input
-          type="email"
-          name="email"
-          placeholder="Enter your Email"
-          onChange={handleInputChange}
-          className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"
-        /></div>
-        <div><label htmlFor="pass" className="block text-sm text-gray-300 mb-1">
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your Email"
+            onChange={handleInputChange}
+            className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"
+          />
+        </div>
+        <div>
+          <label htmlFor="pass" className="block text-sm text-gray-300 mb-1">
             Password
-          </label><input
-          type="password"
-          name="pass"
-          placeholder="Set password"
-          onChange={handleInputChange}
-          className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"        />
+          </label>
+          <input
+            type="password"
+            name="pass"
+            placeholder="Set password"
+            onChange={handleInputChange}
+            className="w-full bg-grey-soft text-white border border-grey-soft rounded-lg px-4 py-3 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-royal-blue transition"
+          />
         </div>
         <button
           onClick={handleFormSubmit}
@@ -85,15 +112,19 @@ const Register = () => {
         >
           Register
         </button>
-        <div className="text-center mt-6"><p className="text-sm text-gray-400">Already have an account?</p>
-        <Link to="/login" className="text-royal-blue hover:text-royal-blue/50">
-          Login here
-        </Link></div>
-        
+        <div className="text-center mt-6">
+          <p className="text-sm text-gray-400">Already have an account?</p>
+          <Link
+            to="/login"
+            className="text-royal-blue hover:text-royal-blue/50"
+          >
+            Login here
+          </Link>
+        </div>
       </div>
       <div className="text-center mt-8 text-gray-400 text-xs">
-          <p>© {new Date().getFullYear()} Enqode. All rights reserved.</p>
-        </div>
+        <p>© {new Date().getFullYear()} Enqode. All rights reserved.</p>
+      </div>
     </div>
   );
 };
