c3c64c82b6ad8953b882556bcf4c33247986366b
Chahatkalra29 <Kalrachahat0@gmail.com>
2025-06-30 14:01:51 +0530

    cors error

--- server/App.js
+++ server/App.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://enqode.vercel.app/",
+    origin: "https://enqode.vercel.app",
     credentials: true,
   })
 );
